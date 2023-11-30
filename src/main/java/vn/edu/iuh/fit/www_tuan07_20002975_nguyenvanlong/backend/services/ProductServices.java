44ee3ccd3fd7554e6b221f77b9dddf6aaf745a08
NguyenLongMoon <longsky0912624119@gmail.com>
2023-12-01 03:58:05 +0700

    cap nhat productpriceservices, productservices

---
+++ src/main/java/vn/edu/iuh/fit/www_tuan07_20002975_nguyenvanlong/backend/services/ProductServices.java
@@ -0,0 +1,28 @@
+package vn.edu.iuh.fit.www_tuan07_20002975_nguyenvanlong.backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import vn.edu.iuh.fit.www_tuan07_20002975_nguyenvanlong.backend.enums.ProductStatus;
+import vn.edu.iuh.fit.www_tuan07_20002975_nguyenvanlong.backend.models.Product;
+import vn.edu.iuh.fit.www_tuan07_20002975_nguyenvanlong.backend.repositories.ProductRepository;
+
+@Service
+public class ProductServices {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Page<Product> findPaging(int page, int pageSize, String sortBy, String sortDirection) {
+        PageRequest pageRequest = PageRequest.of(page-1, pageSize, Sort.by(
+                Sort.Direction.valueOf(sortDirection), sortBy));
+
+//        return productRepository.findAll(pageRequest);
+        return productRepository.findToRender(ProductStatus.ACTIVE, pageRequest);
+    }
+
+    public Integer softDelete(long productId) {
+        return productRepository.updateStatus(productId, ProductStatus.TERMINATED);
+    }
+}
